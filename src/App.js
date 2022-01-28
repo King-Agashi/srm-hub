e0da8569781c47123818bb6620c7746026827891
King-Agashi <rajarshi.agarwal.10122001@gmail.com>
2022-01-28 19:21:50 +0530

    T-T

--- src/App.js
+++ src/App.js
@@ -3,36 +3,45 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="style-container">
-        <div>
-          <h1>
-            Heading - H1
-          </h1>
+      <nav class="navbar navbar-expand-md navbar-dark">
+        <a href="/" class="navbar-brand logo">Srm Hub</a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarMenu">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item"><a href="/" class="nav-link"> Home</a></li>
+            <li class="nav-item"><a href="/notes" class="nav-link"> Notes </a></li>
+            <li class="nav-item"><a href="/restaurants" class="nav-link"> Restaurants </a></li>
+            <li class="nav-item"><a href="#Contact" class="nav-link"> Contact Us </a></li>
+          </ul>
         </div>
-        <div>
-          <h2>
-            Heading - H2
-          </h2>
-        </div>
-        <div>
-          <h3 className="logo">
-            LOGO
-          </h3>
-        </div>
-        <div>
-          <h4>
-            Heading - H4
-          </h4>
-        </div>
-        <div>
-          <p>
-            paragraph
-          </p>
-        </div>
-        <div>
-          <p class="para-small">
-            short-paragraph
-          </p>
+      </nav>
+      
+      <div class="container-fluid h-100 mb-5 mt-5 pt-5">
+        <div class="row top-buffer text-center align-items-center h-100">
+          <div class="col-12 col-md-6">
+            <img id="Bldg" alt="Landing Page Background" class="img-fluid" src="/Assets/quite-town.png"/>
+          </div>
+          <div class="col-12 col-md-6">
+            <div class="font-weight-light">
+              <h1>Welcome To</h1>
+            </div>
+            <div>
+              <h2 id="TypeWriter"> SRM HUB </h2>
+            </div>
+            <div class="font-weight-light casual">
+              <p>Find the best notes and restaurant reviews</p>
+            </div>
+            <div class="Btns row">
+              <div class="col-12 col-md-6 text-lg-right text-center">
+                <a href="/restaurants"><button class="btn btn-outline-light btn-lg mt-3 mb-2"> Restaurants </button> </a>
+              </div>
+              <div class="col-12 col-md-6 text-lg-left text-center">
+                <a href="/notes"><button class="btn btn-outline-light btn-lg mt-3 mb-2"> Notes </button> </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
